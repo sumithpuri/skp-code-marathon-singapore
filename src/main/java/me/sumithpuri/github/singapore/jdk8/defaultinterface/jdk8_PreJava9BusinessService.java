c508b84ca82636a5eeea44ce36222f7f936db0a5
sumithpuri <sumith.puri@yahoo.com>
2018-10-17 01:25:36 +0800

    minor change in licence spellings
    #minor changes in licence grammar/spellings

--- src/main/java/me/sumithpuri/github/singapore/jdk8/defaultinterface/jdk8_PreJava9BusinessService.java
+++ src/main/java/me/sumithpuri/github/singapore/jdk8/defaultinterface/jdk8_PreJava9BusinessService.java
@@ -16,7 +16,7 @@ package me.sumithpuri.github.singapore.jdk8.defaultinterface;
  *
  * Permission is hereby  granted,  free  of  charge, to  any person  obtaining a  copy of  this  software and associated 
  * documentation files (the "Software"), to deal in the  Software without  restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense,and/or sell copies of the Software, and to permit 
+ * rights to use, copy, modify, merge, publish, distribute, sub-license and/or sell copies of the Software and to permit 
  * persons to whom the Software is furnished to do so, subject to the following conditions:
  * 
  * The above copyright notice and this permission notice shall be included in  all copies or substantial portions of the 
